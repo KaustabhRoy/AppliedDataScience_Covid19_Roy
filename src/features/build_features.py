422e8e9db088110d1dd6e57b46277bca621858a0
KaustabhRoy <kroy@rhrk.uni-kl.de>
2022-07-27 23:54:36 +0200

    Update from lectures

---
+++ src/features/build_features.py
@@ -0,0 +1,106 @@
+import numpy as np
+from sklearn import linear_model
+reg = linear_model.LinearRegression(fit_intercept=True)
+import pandas as pd
+
+from scipy import signal
+
+
+def get_doubling_time_via_regression(in_array):
+    ''' Use a linear regression to approximate the doubling rate
+        Parameters:
+        ----------
+        in_array : pandas.series
+        Returns:
+        ----------
+        Doubling rate: double
+    '''
+
+    y = np.array(in_array)
+    X = np.arange(-1,2).reshape(-1, 1)
+
+    assert len(in_array)==3
+    reg.fit(X,y)
+    intercept=reg.intercept_
+    slope=reg.coef_
+
+    return intercept/slope
+
+from scipy import signal
+
+def savgol_filter(df_input,column='Confirmed',window=5):
+    ''' Savgol Filter which can be used in groupby apply function 
+        it ensures that the data structure is kept'''
+    window=5, 
+    degree=1
+    df_result=df_input
+    
+    filter_in=df_input[column].fillna(0) # attention with the neutral element here
+    
+    result=signal.savgol_filter(np.array(filter_in),
+                           5, # window size used for filtering
+                           1)
+    df_result[column+'_filtered']=result
+    return df_result
+
+def rolling_reg(df_input,col='Confirmed'):
+    ''' input has to be a data frame'''
+    ''' return is single series (mandatory for group by apply)'''
+    days_back=3
+    result=df_input[col].rolling(
+                window=days_back,
+                min_periods=days_back).apply(get_doubling_time_via_regression,raw=False)
+    return result 
+
+def calc_filtered_data(df_input,filter_on = 'Confirmed'):
+    must_contain = set(['State','Country',filter_on])
+    assert must_contain.issubset(set(df_input.columns)), 'Error in calc_filtered_data not all columns in data frame'
+
+    df_output=df_input.copy() # we need a copy here otherwise the filter_on column will be overwritten
+
+    pd_filtered_result=df_output[['State','Country',filter_on]].groupby(['State','Country']).apply(savgol_filter)#.reset_index()
+
+    #print('--+++ after group by apply')
+    #print(pd_filtered_result[pd_filtered_result['country']=='Germany'].tail())
+
+    #df_output=pd.merge(df_output,pd_filtered_result[['index',str(filter_on+'_filtered')]],on=['index'],how='left')
+    df_output=pd.merge(df_output,pd_filtered_result[[str(filter_on+'_filtered')]],left_index=True,right_index=True,how='left')
+    #print(df_output[df_output['country']=='Germany'].tail())
+    return df_output.copy()
+
+def calc_doubling_rate(df_input,filter_on='Confirmed'):
+
+    must_contain=set(['State','Country',filter_on])
+    assert must_contain.issubset(set(df_input.columns)), 'Error in calc_filtered_data not all columns in data frame'
+
+
+    pd_DR_result= df_input.groupby(['State','Country']).apply(rolling_reg,filter_on).reset_index()
+
+    pd_DR_result=pd_DR_result.rename(columns={filter_on:filter_on+'_DR',
+                             'level_2':'index'})
+
+    df_output=pd.merge(df_input,pd_DR_result[['index',str(filter_on+'_DR')]],left_index=True,right_on=['index'],how='left')
+    df_output=df_output.drop(columns=['index'])
+
+    return df_output
+
+if __name__ == '__main__':
+    test_data_reg=np.array([2,4,6])
+    result=get_doubling_time_via_regression(test_data_reg)
+    print('the test slope is: '+str(result))
+
+    pd_raw=pd.read_csv('data/processed/COVID_relational_confirmed.csv',sep=';',parse_dates=[0])
+    pd_raw=pd_raw.sort_values('date',ascending=True).copy()
+
+    #test_structure=pd_JH_data[((pd_JH_data['country']=='US')|
+    #                  (pd_JH_data['country']=='Germany'))]
+
+    pd_result_large=calc_filtered_data(pd_raw)
+    pd_result_large=calc_doubling_rate(pd_result_large)
+    pd_result_large=calc_doubling_rate(pd_result_large,'Confirmed_filtered')
+
+
+    mask=pd_result_large['Confirmed']>100
+    pd_result_large['Confirmed_filtered_DR']=pd_result_large['Confirmed_filtered_DR'].where(mask, other=np.NaN)
+    pd_result_large.to_csv('data/processed/COVID_final_set.csv',sep=';',index=False)
+    print(pd_result_large[pd_result_large['Country']=='Germany'].tail())
